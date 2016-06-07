c33e4e8aae57c67de38d3dbbb8148293f2079eca
Jahred Hope <jahred.hope@xero.com>
2016-06-07 18:56:39 +1000

    Disable Add button when input is empty

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,12 +2,28 @@ import React, { Component, PropTypes } from 'react';
 import todoImage from '../todo.png';
 
 export default class AddTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {message: ''};
+  }
+  handleChange(e) {
+    this.setState({message: e.target.value})
+  }
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.handleClick();
+    }
+  }
   render() {
     return (
       <div>
         <img style={{height: '40px', display: 'inline'}} src={todoImage} />
-        <input type='text' ref='input' />
-        <button onClick={e => this.handleClick(e)}>
+        <input type='text' ref='input'
+          onChange={this.handleChange.bind(this)}
+          onKeyPress = {this.handleKeyPress.bind(this)} />
+        <button
+          disabled={!this.state.message}
+          onClick={this.handleClick.bind(this)}>
           Add
         </button>
       </div>
@@ -17,7 +33,7 @@ export default class AddTodo extends Component {
   handleClick(e) {
     const node = this.refs.input;
     const text = node.value.trim();
-    this.props.onAddClick(text);
+    text && this.props.onAddClick(text);
     node.value = '';
   }
 }
